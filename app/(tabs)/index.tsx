ce8fd1dc621d39cc1f44d02dbbbd4f3c8c68585c
Pedro Costa <pedrocosta.lov@gmail.com>
2024-10-26 11:26:36 -0300

    fix scroll view

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,8 @@
-import Evento from '@/interfaces/Evento';
 import ListaEventos from '@/components/ListaEventos';
-import { StyleSheet, Text, View } from 'react-native';
-import HeaderPagina from '@/components/HeaderPagina';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import LayoutBasicoComTitulo from '@/layouts/LayoutBasico';
 
 import data from '@/common/Database'
-import { ScrollView } from 'react-native-gesture-handler';
 import BotaoFlutuante from '@/components/BotaoFlutuante';
 
 export default function HomeScreen() {
