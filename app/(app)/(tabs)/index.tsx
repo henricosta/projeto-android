8135d5ebbd42c39e4b3c13c119810778ea201c9a
Pedro Costa <pedrocosta.lov@gmail.com>
2024-11-04 11:11:28 -0300

    faz tela de listagem consultar api

--- app/(app)/(tabs)/index.tsx
+++ app/(app)/(tabs)/index.tsx
@@ -1,16 +1,40 @@
 import ListaEventos from '@/components/ListaEventos';
 import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native';
 import LayoutBasicoComTitulo from '@/layouts/LayoutBasico';
+import API from '@/common/paths'
+import axios from 'axios';
 
 import data from '@/common/Database'
 import BotaoFlutuante from '@/components/BotaoFlutuante';
+import { Href, router } from 'expo-router';
+import { useEffect, useState } from 'react';
 
 export default function HomeScreen() {
+  const [eventos, setEventos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchEventos = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(API.LISTAR_EVENTOS);
+        setEventos(response.data.events); // Assuming response.data contains the list of eventos
+        console.log(response.data.events)
+      } catch (err: any) {
+        setError(err.message); // Capture error if the request fails
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchEventos();
+  }, []);
   return (
     <SafeAreaView style={styles.container}>
       <View style={{ margin: 8, flex: 1 }}>
-        <ListaEventos listaEventos={data} />
-        <BotaoFlutuante onPress={() => alert('pressed')} />
+        { eventos.length > 0 && <ListaEventos listaEventos={eventos} />}
+        <BotaoFlutuante onPress={() => router.push('/(app)/adicionar/adicionar' as Href)} />
       </View>
     </SafeAreaView>
   );
